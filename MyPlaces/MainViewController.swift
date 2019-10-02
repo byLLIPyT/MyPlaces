d30e222ae3bf119a939040a6cf00a37d9fecbf91
byLLIPyT <alex-tt@mail.ru>
2019-10-02 09:28:33 +0300

    +icon/

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -33,10 +33,14 @@ class MainViewController: UITableViewController {
 
         cell.textLabel?.text = restaurantName[indexPath.row]
         cell.imageView?.image = UIImage.init(named: restaurantName[indexPath.row])
-        
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
+    }
 
     
     // MARK: - Navigation
